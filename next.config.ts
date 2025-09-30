e6f056bf178f25674c174566510cb162859ebb02
Adam <anhle99.work@gmail.com>
2025-09-30 19:21:40 +0700

    [config i18n]

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import { i18n } from "./next-i18next.config";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -13,7 +12,7 @@ const nextConfig: NextConfig = {
       },
     ]
   },
-  i18n: i18n,
+  // i18n: i18n,
 };
 
 export default nextConfig;
